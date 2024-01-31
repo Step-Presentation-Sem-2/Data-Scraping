89d3d4d36292cccd9973a2e58b93709da25e870d
user.email <joelraju@ymail.com>
2024-01-31 08:27:32 -0500

    feat: added factory for different image sources

---
+++ pexels_scraper.py
@@ -0,0 +1,7 @@
+from base_scraper import BaseScraper
+
+
+class PexelsScraper(BaseScraper):
+    def __init__(self):
+        super().__init__()
+        print("PexelsScraper init")
---
+++ pixabay_scraper.py
@@ -0,0 +1,7 @@
+from base_scraper import BaseScraper
+
+
+class PixabayScraper(BaseScraper):
+    def __init__(self):
+        super().__init__()
+        print("PexelsScraper init")
