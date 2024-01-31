0e40434cf94484ceb545e99210b2cf9cb1b7b1dc
user.email <joelraju@ymail.com>
2024-01-31 08:27:32 -0500

    feat: added factory for different image sources

---
+++ base_scraper.py
@@ -0,0 +1,14 @@
+class BaseScraper:
+    "scraper using the "
+
+    def __init__(self):
+        print("initializing")
+
+    def scrape_images(self):
+        print("scrape_images")
+
+    def scrape_image(self):
+        print("scrape_images")
+
+    def save_image(self):
+        print("save_images")
