844d584beea69f98e116e1875a0f67d7e023f5e3
Joel Raju <Joel-Raju@users.noreply.github.com>
2024-02-01 05:32:20 +0530

    Revert "Abstractions for scraping different image sources"

--- five_hundred_px_scraper.py
+++
@@ -1,7 +0,0 @@
-from base_scraper import BaseScraper
-
-
-class FiveHundredPxScraper(BaseScraper):
-    def __init__(self):
-        super().__init__()
-        print("PexelsScraper init")
--- image_source.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class ImageSource(Enum):
-    PEXELS = 1
-    PIXABAY = 2
-    UNSPLASH = 3
-    FIVE_HUNDRED_PX = 4
