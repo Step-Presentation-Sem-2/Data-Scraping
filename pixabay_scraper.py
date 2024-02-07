4b15d2de7909451bb551e8c6d9e8d98ff8ad53e2
joel-raju <joelraju@ymail.com>
2024-02-07 15:47:34 -0500

    feat: added thispersondoesnotexist scraper

--- pixabay_scraper.py
+++ pixabay_scraper.py
@@ -1,3 +1,4 @@
+import urllib.request, io
 from base_scraper import BaseScraper
 
 
@@ -5,3 +6,6 @@ class PixabayScraper(BaseScraper):
     def __init__(self):
         super().__init__()
         print("PexelsScraper init")
+
+    def scrape_images(self):
+        self.save_image()
